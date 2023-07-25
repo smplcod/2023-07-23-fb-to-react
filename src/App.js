170ee0f3f77520f389a4552638ae3de1fcd333f1
smplcod <mplcod2021@gmail.com>
2023-07-25 21:06:07 +0600

    db deleted

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { db, getDbRef } from "./firebase";
+import { getDbRef } from "./firebase";
 import { onValue, set } from "firebase/database";
 
 const App = () => {
